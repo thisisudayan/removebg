108052a8f428ff0ce1b0d16b26e0c89a5e7a8bd7
Udayan Basak <117582308+thisisudayan@users.noreply.github.com>
2023-02-11 10:00:06 +0600

    af

---
+++ main.py
@@ -0,0 +1,30 @@
+# modules for remove background and request
+from rembg import remove
+import requests
+from PIL import Image
+
+#module for create server
+import tornado.web
+import tornado.ioloop
+
+class basicRequestHandler(tornado.web.RequestHandler):
+    def get(self):
+        imageurl = self.get_argument("image")
+        color = self.get_argument("color")
+        outputPath = 'output.png'
+        input = Image.open(requests.get(imageurl, stream=True).raw)
+        output = remove(input)
+        output.save(outputPath)
+        with open('output.png', 'rb') as file: 
+            self.write(file.read())
+        self.set_header("Content-type",  "image/png")
+
+
+if __name__ == "__main__":
+    app = tornado.web.Application([
+        (r"/", basicRequestHandler)
+    ])
+
+    app.listen(8888)
+    print("I'm listening on port 8888")
+    tornado.ioloop.IOLoop.current().start()
